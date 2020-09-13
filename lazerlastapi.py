549a719348491fcd4e9ff90533b68223b8107e74
Infqq <57950063+Infqq@users.noreply.github.com>
2020-09-13 12:40:24 +0400

    Delete lazerlastapi.py

--- lazerlastapi.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def prison(act, token):
-    url = "http://api.lazerlast.ru/prison.php?player="+act+"&token="+token+"&format=json"
-    r = requests.get(url).json()
-    prison.result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
