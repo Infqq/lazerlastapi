05b2cb3d3761008a00707a912c12ee67aa2eca86
Infqq <57950063+Infqq@users.noreply.github.com>
2020-09-14 15:43:38 +0400

    Update lazerlastapi.py

--- lazerlastapi/lazerlastapi.py
+++ lazerlastapi/lazerlastapi.py
@@ -3,16 +3,12 @@ import requests
 print('LLAPI was started')
 print()
 
-class API:
-	def __init__(self, token: str, nick: str):
-		self._token = token
-		self.nick = nick
-		url = "http://api.lazerlast.ru/prison.php?player="+self.nick+"&token="+self._token+"&format=json"
-		r = requests.get(url).json()
-		result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
+class PRISON:
+        def __init__(self, token: str):
+                self._token = token
 
-class Prison1():
-    def prison(act, token):
-        url = "http://api.lazerlast.ru/prison.php?player="+act+"&token="+token+"&format=json"
-        r = requests.get(url).json()
-        prison.result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
+        def get(self, name: str):
+                url = "http://api.lazerlast.ru/prison.php?player="+name+"&token="+self._token+"&format=json"
+                r = requests.get(url).json()
+                result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
+                return result
