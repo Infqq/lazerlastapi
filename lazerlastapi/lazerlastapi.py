1ba1f2a58cf375ab11b3d77db5ba95fb56ba2294
Infqq <57950063+Infqq@users.noreply.github.com>
2020-09-14 18:22:55 +0400

    Add lw, lobby, mlf
    Добавлены типы: лобби, лакиварс, млф

--- lazerlastapi/lazerlastapi.py
+++ lazerlastapi/lazerlastapi.py
@@ -12,3 +12,33 @@ class PRISON:
                 r = requests.get(url).json()
                 result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
                 return result
+
+class MLF:
+        def __init__(self, token: str):
+                self._token = token
+
+        def get(self, name: str):
+                url = "http://api.lazerlast.ru/mlf.php?player="+name+"&token="+self._token+"&format=json"
+                r = requests.get(url).json()
+                result = 'Ник: ' + r['username'] + '\n$/сек: ' + r['production'] + '\nБаланс: ' + r['balance']
+                return result
+
+class LW:
+        def __init__(self, token: str):
+                self._token = token
+
+        def get(self, name: str):
+                url = "http://api.lazerlast.ru/prison.php?player="+name+"&token="+self._token+"&format=json"
+                r = requests.get(url).json()
+                result = 'Ник: ' + r['username'] + '\nБаланс: ' + r['money'] + '\nУбийств: ' + r['kills'] + '\nСмертей: ' + r['deaths'] + '\nПобеды: ' + r['wins']
+                return result
+        
+class LOBBY:
+        def __init__(self, token: str):
+                self._token = token
+
+        def get(self, name: str):
+                url = "http://api.lazerlast.ru/prison.php?player="+name+"&token="+self._token+"&format=json"
+                r = requests.get(url).json()
+                result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nКоины: ' + r['coin'] + '\nУровень: ' + r['level']
+                return result
