9eda3887cda97075e4fc9e74be306a9740af3d36
Infqq <57950063+Infqq@users.noreply.github.com>
2020-10-09 17:13:33 +0400

    Update lazerlastapi.py

--- lazerlastapi/lazerlastapi.py
+++ lazerlastapi/lazerlastapi.py
@@ -64,3 +64,23 @@ class PLAYER:
                 except:
                         result = r['error']
                 return result
+
+class TEST:
+        def __init__(self, token: str):
+                self._token = token
+
+        def get(self, name: str):
+                url = "http://api.lazerlast.ru/prison.php?player="+name+"&token="+self._token+"&format=json"
+                r = requests.get(url).json()
+                try:
+                        result = 'Ник: ' + r['username'] + '\nОпыт: ' + r['exp'] + '\nБаланс: ' + r['balance'] + '\nУровень: ' + r['level'] + '\nБлоки: ' + r['blocks'] + '\nКристаллы: ' + r['crist'] + '\nКрысы: ' + r['rats']
+                        username = 'Ник: ' + r['username']
+                        exp = '\nОпыт: ' + r['exp']
+                        balance = '\nБаланс: ' + r['balance']
+                        level = '\nУровень: ' + r['level']
+                        blocks = '\nБлоки: ' + r['blocks']
+                        crist = '\nКристаллы: ' + r['crist']
+                        rats = '\nКрысы: ' + r['rats']
+                except:
+                        result = r['error']
+                return result
