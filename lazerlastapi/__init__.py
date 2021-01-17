10fc7f5747ca650e8fc9d3d952cb3db97763c9b1
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-01-17 19:56:27 +0400

    lil fix 2

--- lazerlastapi/__init__.py
+++ lazerlastapi/__init__.py
@@ -1,7 +1,3 @@
 import lazerlastapi
-from .lazerlastapi import PRISON
-from .lazerlastapi import LOBBY
-from .lazerlastapi import LW
-from .lazerlastapi import MLF
 from .lazerlastapi import PLAYER
 from .lazerlastapi import *
