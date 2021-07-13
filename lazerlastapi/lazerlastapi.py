038888c6490c609248b214c7b8893fd15e72dd8a
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-07-13 15:23:55 +0400

    indexation fix

--- lazerlastapi/lazerlastapi.py
+++ lazerlastapi/lazerlastapi.py
@@ -3,11 +3,12 @@ import requests
 
 class lazerlastapi:
     def __init__(self, token: str):
-            self._token = token
+        self._token = token
 
     def player(self, nickname: str):
-            r = requests.get(f"http://api.lazerlast.ru/player.php?token={self._token}&player={nickname}").json()
-            return r
+        r = requests.get(f"http://api.lazerlast.ru/player.php?token={self._token}&player={nickname}").json()
+        return r
+    
     def token(self):
         r = requests.get(f"http://api.lazerlast.ru/token.php?token={self._token}").json()
         return r
